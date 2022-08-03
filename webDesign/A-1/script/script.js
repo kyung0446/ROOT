43411b2c529be7a6cc901ee350db0c052c69539c
kyung0446 <kyung0446@gmail.com>
2022-08-03 09:00:31 +0900

    webdesign

---
+++ webDesign/A-1/script/script.js
@@ -0,0 +1,59 @@
+$(
+    function(){
+        //메뉴 슬라이드
+        $('.subMenu').slideUp(0);
+        // $('nav').hover(
+        //     //over
+        //     function(){
+        //         $('.subMenu').stop().slideDown('fast');
+        //     },
+        //     //out
+        //     function(){
+        //         $('.subMenu').stop().slideUp('fast');
+        //     }
+        // );
+        $('.mainMenu').hover(
+            //over
+            function(){
+                $(this).children('.subMenu').stop().slideDown('fast');
+            },
+            //out
+            function(){
+                $(this).children('.subMenu').stop().slideUp('fast');
+            }
+        );
+
+        //상하 슬라이드
+        const TOTAL_IMAGE = 2;  
+        let currentIndex = 0;  /* 0,1,2*/
+        let slidePos = 0;
+        setInterval(loopSlide,3000);
+        function loopSlide(){
+            if( currentIndex < TOTAL_IMAGE ) {
+                currentIndex++;
+            } else{
+                currentIndex = 0;
+            }
+            // currentIndex = (currentIndex==TOTAL_IMAGE) ? 0 : ++currentIndex;
+            slidePos = currentIndex*(-300)+'px';
+            $('.slide-list').animate({top:slidePos},1000);
+        }
+
+        //tab menu
+        $('.tabMenu li').click(function(){
+            let pos = $(this).index();
+            $('.tabItem ul').hide().removeClass('on');
+            $('.tabItem ul').eq(pos).show().addClass('on');
+            //
+            $('.tabMenu li').removeClass('on');
+            $(this).addClass('on');
+        });
+
+        //popup
+        $('.notice li:nth-child(1)').click(
+            function(){
+                $('.popupBox').show();
+            }
+        );
+    }
+);
