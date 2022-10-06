ea610a7041f95629f0a2cdb4b62e6149ab3ba282
소은경 <83527824+kyung0446@users.noreply.github.com>
2022-10-06 17:41:07 +0900

    update

--- PROJECT/apple-clone/script/script.js
+++ PROJECT/apple-clone/script/script.js
@@ -9,9 +9,6 @@ const setCurrentScene = () => {
         }
     }
 }
-const handlerScroll = () => {
-    yOffset = window.pageYOffset;
-}
 const setSecHeight = () => {
     for( let i=0 ; i<sceneInfo.length ; i++ ){
         sceneInfo[i].scrollHeight = sceneInfo[i].heightCnt * window.innerHeight;
@@ -19,12 +16,54 @@ const setSecHeight = () => {
     }
     setCurrentScene();
 }
+const calc_values = (values,ratio) => {
+    let calc=0;
+    calc = ratio*(values[1]-values[0])+values[0];
+    return calc;
+}
+const sectionAniPlay = (prevHeight) => {
+    const currentYOffset = yOffset - prevHeight;
+    let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
+    //
+    const tagObjs = sceneInfo[currentScene].objs;
+    const values = sceneInfo[currentScene].values;
+    switch( currentScene ) {
+        case 0 :  //section-0 
+            if( scrollRatio <= 0.2 ) {
+                tagObjs.msgA.style.opacity = calc_values(values.msgA_opacity_in, scrollRatio);
+            } else{
+                tagObjs.msgA.style.opacity = calc_values(values.msgA_opacity_out, scrollRatio);
+            }
+            break;
+        case 1 :  //section-1
+            break;
+        case 2 :  //section-1
+            break;
+        case 3 :  //section-1
+            break;
+    }
+}
+const handlerScroll = () => {
+    yOffset = window.pageYOffset;
+    //스크롤이 발생될 때 현재 보여지는 section 값을 가져오도록 함.
+    let prevHeight = 0;
+    for( let i=0 ; i<currentScene ; i++ ){
+        prevHeight += sceneInfo[i].scrollHeight;
+    }
+    if( yOffset > prevHeight + sceneInfo[currentScene].scrollHeight ){
+        currentScene++;
+    }
+    if( yOffset < prevHeight ) {
+        currentScene--;
+    }
+    document.body.setAttribute('id',`show-section-${currentScene}`);
+    sectionAniPlay(prevHeight);
+}
 const init = () => {
     //각각의 섹션의 height 설정
     setSecHeight();
     //초기화
     window.addEventListener('scroll',()=>{
-        //스크롤이 발생될 때 현재 보여지는 section 값을 가져오도록 함.
         handlerScroll();
     });
 }
--- PROJECT/apple-clone/script/variable.js
+++ PROJECT/apple-clone/script/variable.js
@@ -7,8 +7,22 @@ const sceneInfo = [
         scrollHeight : 0,  
         objs : {
             container : document.querySelector('#scroll-section-0'),
+            msgA : document.querySelector('#scroll-section-0 > .a'),
+            msgB : document.querySelector('#scroll-section-0 > .b'),
+            msgC : document.querySelector('#scroll-section-0 > .c'),
+            msgD : document.querySelector('#scroll-section-0 > .d'),
         },
-
+        values : {
+            // msgA_opacity_in : [0,1,{start:0.2, end:0.3}],
+            // msgA_position_in : [30,0,{start:0.2, end:0.3}],
+            // msgA_opacity_out : [1,0,{start:0.3, end:0.4}],
+            // msgA_position_out : [0,-30,{start:0.3, end:0.4}],
+            msgA_opacity_in : [0,1],
+            msgA_position_in : [30,0],
+            msgA_opacity_out : [1,0],
+            msgA_position_out : [0,-30],
+        },
+        imgCount : 117,
     },
     {//section-1
         heightCnt : 2,
