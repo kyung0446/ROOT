8da8943bfd4f068ac995353efd1f4d5de8981b1b
소은경 <83527824+kyung0446@users.noreply.github.com>
2022-10-11 09:09:30 +0900

    clone update

--- PROJECT/apple-clone/script/script.js
+++ PROJECT/apple-clone/script/script.js
@@ -16,9 +16,25 @@ const setSecHeight = () => {
     }
     setCurrentScene();
 }
-const calc_values = (values,ratio) => {
+const partAniPlay = (values,currentYOffset) => {
     let calc=0;
-    calc = ratio*(values[1]-values[0])+values[0];
+    const scrollHeight = sceneInfo[currentScene].scrollHeight;
+    const scrollRatio = currentYOffset / scrollHeight;
+    if( values.length >= 3 ){
+        const start = values[2].start * scrollHeight;
+        const end = values[2].end * scrollHeight;
+        const partHeight = end - start;
+        const partRatio =  (currentYOffset-start) / partHeight;
+        if( currentYOffset >= start && currentYOffset <= end) {
+            calc = partRatio*(values[1]-values[0])+values[0];
+        } else if( currentYOffset < start ) {
+            calc = values[0];
+        } else if( currentYOffset > end ) {
+            calc = values[1];
+        }
+    } else {
+        calc = scrollRatio*(values[1]-values[0])+values[0];
+    }    
     return calc;
 }
 const sectionAniPlay = (prevHeight) => {
@@ -29,17 +45,50 @@ const sectionAniPlay = (prevHeight) => {
     const values = sceneInfo[currentScene].values;
     switch( currentScene ) {
         case 0 :  //section-0 
-            if( scrollRatio <= 0.2 ) {
-                tagObjs.msgA.style.opacity = calc_values(values.msgA_opacity_in, scrollRatio);
-            } else{
-                tagObjs.msgA.style.opacity = calc_values(values.msgA_opacity_out, scrollRatio);
-            }
+            //canvas load
+            let seq = Math.round(partAniPlay(values.images_seq, currentYOffset));
+            if( seq < 0 ) seq = 0;
+            tagObjs.context.drawImage(tagObjs.videoImages[seq],0,0);
+            tagObjs.canvas.style.opacity = partAniPlay(values.canvas_opacity_out, currentYOffset);
+            //messageA
+            if( scrollRatio <= 0.22 ) {
+                tagObjs.msgA.style.opacity = partAniPlay( values.msgA_opacity_in , currentYOffset );
+                tagObjs.msgA.style.transform = `translateY(${partAniPlay( values.msgA_position_in , currentYOffset )}px)`;
+            } else {
+                tagObjs.msgA.style.opacity = partAniPlay( values.msgA_opacity_out , currentYOffset );
+                tagObjs.msgA.style.transform = `translateY(${partAniPlay( values.msgA_position_out , currentYOffset )}px)`;
+            }   
+            //messageB
+            if( scrollRatio <= 0.32 ) {
+                tagObjs.msgB.style.opacity = partAniPlay( values.msgB_opacity_in , currentYOffset );
+                tagObjs.msgB.style.transform = `translateY(${partAniPlay( values.msgB_position_in , currentYOffset )}px)`;
+            } else {
+                tagObjs.msgB.style.opacity = partAniPlay( values.msgB_opacity_out , currentYOffset );
+                tagObjs.msgB.style.transform = `translateY(${partAniPlay( values.msgB_position_out , currentYOffset )}px)`;
+            } 
+            //messageC
+            if( scrollRatio <= 0.42 ) {
+                tagObjs.msgC.style.opacity = partAniPlay( values.msgC_opacity_in , currentYOffset );
+                tagObjs.msgC.style.transform = `translateY(${partAniPlay( values.msgC_position_in , currentYOffset )}px)`;
+            } else {
+                tagObjs.msgC.style.opacity = partAniPlay( values.msgC_opacity_out , currentYOffset );
+                tagObjs.msgC.style.transform = `translateY(${partAniPlay( values.msgC_position_out , currentYOffset )}px)`;
+            } 
+            //messageD
+            if( scrollRatio <= 0.52 ) {
+                tagObjs.msgD.style.opacity = partAniPlay( values.msgD_opacity_in , currentYOffset );
+                tagObjs.msgD.style.transform = `translateY(${partAniPlay( values.msgD_position_in , currentYOffset )}px)`;
+            } else {
+                tagObjs.msgD.style.opacity = partAniPlay( values.msgD_opacity_out , currentYOffset );
+                tagObjs.msgD.style.transform = `translateY(${partAniPlay( values.msgD_position_out , currentYOffset )}px)`;
+            } 
             break;
         case 1 :  //section-1
             break;
         case 2 :  //section-1
             break;
         case 3 :  //section-1
+
             break;
     }
 }
@@ -59,14 +108,51 @@ const handlerScroll = () => {
     document.body.setAttribute('id',`show-section-${currentScene}`);
     sectionAniPlay(prevHeight);
 }
+
+const setCanvasImages = () => {
+    let imgElem;
+    let heightRatio;
+    for( let j=0 ; j<sceneInfo.length ; j++ ) {
+        //이미지 객제 삽입
+        let count = sceneInfo[j].imgCount;
+        if( count <= 0 ) continue;
+        for( let i=0 ; i<count ; i++ ) {
+            imgElem = new Image();
+            imgElem.src = `./images/seq_${j}/${i}.jpg`;
+            sceneInfo[j].objs.videoImages.push(imgElem);
+        }
+        heightRatio = window.innerHeight / sceneInfo[j].objs.canvas.height;
+        if( j === 0 ) {
+            sceneInfo[j].objs.canvas.style.transform = `translate(-50%,-50%) scale(${heightRatio})`;
+        }
+        else {
+            sceneInfo[j].objs.canvas.style.transform = `scale(${heightRatio})`;
+        }
+        
+        sceneInfo[j].objs.videoImages[0].onload = () => {
+            sceneInfo[j].objs.context.drawImage(sceneInfo[j].objs.videoImages[0],0,0);
+        } 
+    }
+}
+const menuCheck = () => {
+    if( yOffset > 45 ) {
+        document.querySelector('.local-nav').classList.add('local-nav-fixed');
+    }else {
+        document.querySelector('.local-nav').classList.remove('local-nav-fixed');
+    }
+}
 const init = () => {
     //각각의 섹션의 height 설정
     setSecHeight();
+    //캔버스 이미지 로드
+    setCanvasImages();
+  
     //초기화
     window.addEventListener('scroll',()=>{
         handlerScroll();
+        menuCheck();
     });
-}
 
+}
 window.addEventListener('load',init);
 window.addEventListener('resize',setSecHeight);
--- PROJECT/apple-clone/script/variable.js
+++ PROJECT/apple-clone/script/variable.js
@@ -5,28 +5,42 @@ const sceneInfo = [
     {//section-0
         heightCnt : 5,   //window.innerHeight 의 몇배로 height설정
         scrollHeight : 0,  
+        imgCount : 117,
         objs : {
             container : document.querySelector('#scroll-section-0'),
             msgA : document.querySelector('#scroll-section-0 > .a'),
             msgB : document.querySelector('#scroll-section-0 > .b'),
             msgC : document.querySelector('#scroll-section-0 > .c'),
             msgD : document.querySelector('#scroll-section-0 > .d'),
+            canvas : document.querySelector('#canvas-0'),
+            context : document.querySelector('#canvas-0').getContext('2d'),
+            videoImages : [],
         },
         values : {
-            // msgA_opacity_in : [0,1,{start:0.2, end:0.3}],
-            // msgA_position_in : [30,0,{start:0.2, end:0.3}],
-            // msgA_opacity_out : [1,0,{start:0.3, end:0.4}],
-            // msgA_position_out : [0,-30,{start:0.3, end:0.4}],
-            msgA_opacity_in : [0,1],
-            msgA_position_in : [30,0],
-            msgA_opacity_out : [1,0],
-            msgA_position_out : [0,-30],
+            images_seq : [0,116],
+            canvas_opacity_out : [1,0,{start:0.9, end:1}],
+            msgA_opacity_in : [0,1,{start:0.2, end:0.25}],
+            msgA_position_in : [30,0,{start:0.2, end:0.25}],
+            msgA_opacity_out : [1,0,{start:0.25, end:0.3}],
+            msgA_position_out : [0,-30,{start:0.25, end:0.3}],
+            msgB_opacity_in : [0,1,{start:0.3, end:0.35}],
+            msgB_position_in : [30,0,{start:0.3, end:0.35}],
+            msgB_opacity_out : [1,0,{start:0.35, end:0.4}],
+            msgB_position_out : [0,-30,{start:0.35, end:0.4}],
+            msgC_opacity_in : [0,1,{start:0.4, end:0.45}],
+            msgC_position_in : [30,0,{start:0.4, end:0.45}],
+            msgC_opacity_out : [1,0,{start:0.45, end:0.5}],
+            msgC_position_out : [0,-30,{start:0.45, end:0.5}],
+            msgD_opacity_in : [0,1,{start:0.5, end:0.55}],
+            msgD_position_in : [30,0,{start:0.5, end:0.55}],
+            msgD_opacity_out : [1,0,{start:0.55, end:0.6}],
+            msgD_position_out : [0,-30,{start:0.55, end:0.5}],
         },
-        imgCount : 117,
     },
     {//section-1
         heightCnt : 2,
         scrollHeight : 0,
+        imgCount : 0 ,
         objs : {
             container : document.querySelector('#scroll-section-1'),
         }  
@@ -34,15 +48,24 @@ const sceneInfo = [
     {//section-2
         heightCnt : 4,
         scrollHeight : 0,
+        imgCount : 117 , 
         objs : {
             container : document.querySelector('#scroll-section-2'),
+            canvas : document.querySelector('#canvas-2'),
+            context : document.querySelector('#canvas-2').getContext('2d'),
+            videoImages : [],
         }  
     },
     {//section-3
         heightCnt : 5,
         scrollHeight : 0,
+        imgCount : 2,
         objs : {
             container : document.querySelector('#scroll-section-3'),
-        }  
+            canvas : document.querySelector('#canvas-3'),
+            context : document.querySelector('#canvas-3').getContext('2d'),
+            videoImages : [],
+        },
+        values :{}, 
     },
 ];
